1656bdddb8eae490b45ea50615280319a23a46c2
Giorgi <giorgigrigolishvili@gmail.com>
2025-12-11 21:13:52 +0400

    fron-end finished

--- frontend/src/components/Result.tsx
+++ frontend/src/components/Result.tsx
@@ -17,65 +17,88 @@ function Result({ result }: { result: any }) {
     );
   }
 
-  if (result.operation === "determinant") {
+  if (result.operation === "Determinant") {
     return (
       <div className="text-center p-8 bg-white rounded-lg shadow">
         <h3 className="text-[2.4rem] font-bold mb-4">
           Result - {result.operation}
         </h3>
-        <p className="text-3xl font-bold">{result.result}</p>
+        <h1
+          className="mr-[2.4rem] text-[2.4rem] font-bold
+        flex items-center gap-[0.8rem]"
+        >
+          {result.matrix_used === "matrix_a" ? "A" : "B"} =
+          <p className="text-3xl font-bold">{result.result}</p>
+        </h1>
       </div>
     );
   }
 
   return (
-    <div className="text-center p-8 bg-white rounded-lg shadow max-w-full overflow-x-auto">
-      <h3 className="text-[2.4rem] font-bold mb-4">
-        Result - {result.operation}
-      </h3>
-      <div className="inline-flex items-center font-mono text-lg">
-        <div
-          className="relative mr-4"
-          style={{
-            width: "8px",
-            height: `${result.result.length * 3 + 2}rem`,
-          }}
-        >
-          <div className="absolute top-0 left-0 w-full h-1 bg-black"></div>
-          <div className="absolute top-0 left-0 w-1 h-full bg-black"></div>
-          <div className="absolute bottom-0 left-0 w-full h-1 bg-black"></div>
-        </div>
+    <div>
+      <div className="text-center p-8 bg-white rounded-lg shadow max-w-full overflow-x-auto">
+        <h3 className="text-[2.4rem] font-bold mb-4">
+          Result - {result.operation}
+        </h3>
+        <div className="inline-flex items-center font-mono text-lg">
+          {result.matrix_used === "matrix_a" && (
+            <h1 className="mr-[2.4rem] text-[2.4rem] font-bold">A =</h1>
+          )}
+          {result.matrix_used === "matrix_b" && (
+            <h1 className="mr-[2.4rem] text-[2.4rem] font-bold">B =</h1>
+          )}
+          {result.matrix_used === "A+B" && (
+            <h1 className="mr-[2.4rem] text-[2.4rem] font-bold">A+B=</h1>
+          )}
+          {result.matrix_used === "A-B" && (
+            <h1 className="mr-[2.4rem] text-[2.4rem] font-bold">A-B=</h1>
+          )}
+          {result.matrix_used === "AB" && (
+            <h1 className="mr-[2.4rem] text-[2.4rem] font-bold">AB=</h1>
+          )}
+          <div
+            className="relative mr-4"
+            style={{
+              width: "8px",
+              height: `${result.result.length * 3 + 2}rem`,
+            }}
+          >
+            <div className="absolute top-0 left-0 w-full h-1 bg-black"></div>
+            <div className="absolute top-0 left-0 w-1 h-full bg-black"></div>
+            <div className="absolute bottom-0 left-0 w-full h-1 bg-black"></div>
+          </div>
 
-        <div
-          className="flex flex-col gap-[1.6rem] py-4
+          <div
+            className="flex flex-col gap-[1.6rem] py-4
           text-[1.6rem] font-[600]
           pl-[1.5rem]"
-        >
-          {result.result.map((row: number[], rowIndex: number) => (
-            <div
-              key={rowIndex}
-              className="grid gap-[1.6rem] text-left"
-              style={{ gridTemplateColumns: `repeat(${row.length}, auto)` }}
-            >
-              {row.map((cell: number, colIndex: number) => (
-                <span key={colIndex} className="min-w-[4rem]">
-                  {Number.isInteger(cell) ? cell : cell.toFixed(3)}
-                </span>
-              ))}
-            </div>
-          ))}
-        </div>
+          >
+            {result.result.map((row: number[], rowIndex: number) => (
+              <div
+                key={rowIndex}
+                className="grid gap-[1.6rem] text-left"
+                style={{ gridTemplateColumns: `repeat(${row.length}, auto)` }}
+              >
+                {row.map((cell: number, colIndex: number) => (
+                  <span key={colIndex} className="min-w-[4rem]">
+                    {Number.isInteger(cell) ? cell : cell.toFixed(3)}
+                  </span>
+                ))}
+              </div>
+            ))}
+          </div>
 
-        <div
-          className="relative"
-          style={{
-            width: "8px",
-            height: `${result.result.length * 3 + 2}rem`,
-          }}
-        >
-          <div className="absolute top-0 right-0 w-full h-1 bg-black"></div>
-          <div className="absolute top-0 right-0 w-1 h-full bg-black"></div>
-          <div className="absolute bottom-0 right-0 w-full h-1 bg-black"></div>
+          <div
+            className="relative"
+            style={{
+              width: "8px",
+              height: `${result.result.length * 3 + 2}rem`,
+            }}
+          >
+            <div className="absolute top-0 right-0 w-full h-1 bg-black"></div>
+            <div className="absolute top-0 right-0 w-1 h-full bg-black"></div>
+            <div className="absolute bottom-0 right-0 w-full h-1 bg-black"></div>
+          </div>
         </div>
       </div>
     </div>
